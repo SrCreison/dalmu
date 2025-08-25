8a63e38d2fe1a9ec23661b4e56d062526f9229a9
SrCreison <87103932+SrCreison@users.noreply.github.com>
2025-08-25 08:50:28 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,26 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/\S+@\S+\.\S+/, 'is invalid'],
+  },
+  image: {
+    type: String,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('User', UserSchema);
