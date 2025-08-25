ee6d72520c48bee35aa29a6076479dc6ba71ba6b
SrCreison <87103932+SrCreison@users.noreply.github.com>
2025-08-25 08:51:13 -0300

    Update eventRoutes.js

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { createEvent, getEventByCode } = require('../controllers/eventController');
 
+// Futuramente, protegeremos a rota de criação com um middleware de autenticação
+// Ex: router.post('/', authMiddleware, createEvent);
+
+router.post('/', createEvent);
+router.get('/:eventCode', getEventByCode);
+
+module.exports = router;
