fc63e0c737835e70f90ba12ce8fa311954803d64
SrCreison <87103932+SrCreison@users.noreply.github.com>
2025-08-25 08:50:49 -0300

    Update Event.js

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,1 +1,36 @@
+const mongoose = require('mongoose');
+const { customAlphabet } = require('nanoid');
 
+const nanoid = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 6);
+
+const SongSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  artist: { type: String, required: true },
+  spotifyId: { type: String },
+  youtubeId: { type: String },
+}, { _id: true });
+
+const EventSchema = new mongoose.Schema({
+  eventName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  eventCode: {
+    type: String,
+    required: true,
+    unique: true,
+    default: () => nanoid(),
+  },
+  organizer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  approvedSongs: [SongSchema],
+  suggestedSongs: [SongSchema],
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Event', EventSchema);
